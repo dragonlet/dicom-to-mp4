c503dd3ef3b4bbde507964a82c234006fc45ff4b
sleepingkirby <sleepingkirby@Minikirby>
2015-01-20 15:19:19 -0800

    first push

---
+++ cs_proj05_dicom2mov/cs_proj05_dicom2mov/conv.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_proj05_dicom2mov
+{
+    class conv
+    {
+    }
+}
