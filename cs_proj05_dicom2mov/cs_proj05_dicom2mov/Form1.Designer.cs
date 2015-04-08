a119848f5ff89bca1d675b2d9886953de75bc515
unknown <sleepingkirby@UFOKirby.megapath.biz>
2015-04-07 22:09:40 -0700

    dcm class made. Has many properties. mainly patientName, dateOfScan and timeOfScan, which can be appended for scan names. Also reworked the appdata/Dicom Folders so that the IM_00XX files are in the "dicoms" folder. When philips want to incorporate it, they can change that to "Somepath/EPIQ or whatever name/DICOM/". We no longer need to care about that. The gui.getDicom will need to be reworked. Should be a lot simpler now. Just sys.getFiles(dicoms_path) and list them.
    Former-commit-id: 85c499985ff034ff1c19896902628d3337337a51

--- cs_proj05_dicom2mov/cs_proj05_dicom2mov/Form1.Designer.cs
+++ cs_proj05_dicom2mov/cs_proj05_dicom2mov/Form1.Designer.cs
@@ -28,99 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.selectListDicom = new System.Windows.Forms.CheckedListBox();
-            this.dropdownProfiles = new System.Windows.Forms.ComboBox();
-            this.flowLayoutPanelConvertButton = new System.Windows.Forms.FlowLayoutPanel();
-            this.buttonConvert = new System.Windows.Forms.Button();
-            this.checklistMovieFiles = new System.Windows.Forms.CheckedListBox();
-            this.flowlayoutMoveButton = new System.Windows.Forms.FlowLayoutPanel();
-            this.buttonMoveTo = new System.Windows.Forms.Button();
-            this.DriveList = new System.Windows.Forms.CheckedListBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.populateDrives = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1.SuspendLayout();
-            this.flowLayoutPanelConvertButton.SuspendLayout();
-            this.flowlayoutMoveButton.SuspendLayout();
+            this.flowlayoutMoveButton = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonMoveTo = new System.Windows.Forms.Button();
+            this.flowLayoutPanelConvertButton = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.DriveList = new System.Windows.Forms.CheckedListBox();
+            this.checklistMovieFiles = new System.Windows.Forms.CheckedListBox();
+            this.dropdownProfiles = new System.Windows.Forms.ComboBox();
+            this.selectListDicom = new System.Windows.Forms.CheckedListBox();
             this.flowLayoutPanel1.SuspendLayout();
+            this.flowlayoutMoveButton.SuspendLayout();
+            this.flowLayoutPanelConvertButton.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // flowLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 4;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Controls.Add(this.selectListDicom, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.dropdownProfiles, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanelConvertButton, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.checklistMovieFiles, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.flowlayoutMoveButton, 2, 2);
-            this.tableLayoutPanel1.Controls.Add(this.DriveList, 3, 1);
-            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 3, 2);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1.698514F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.1104F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(876, 471);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.Controls.Add(this.populateDrives);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(660, 426);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(213, 42);
+            this.flowLayoutPanel1.TabIndex = 6;
             // 
-            // selectListDicom
+            // populateDrives
             // 
-            this.selectListDicom.CheckOnClick = true;
-            this.selectListDicom.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.selectListDicom.FormattingEnabled = true;
-            this.selectListDicom.Location = new System.Drawing.Point(3, 3);
-            this.selectListDicom.Name = "selectListDicom";
-            this.tableLayoutPanel1.SetRowSpan(this.selectListDicom, 3);
-            this.selectListDicom.Size = new System.Drawing.Size(213, 465);
-            this.selectListDicom.TabIndex = 0;
-            // 
-            // dropdownProfiles
-            // 
-            this.dropdownProfiles.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dropdownProfiles.FormattingEnabled = true;
-            this.dropdownProfiles.Items.AddRange(new object[] {
-            "preset1",
-            "preset2"});
-            this.dropdownProfiles.Location = new System.Drawing.Point(222, 3);
-            this.dropdownProfiles.Name = "dropdownProfiles";
-            this.dropdownProfiles.Size = new System.Drawing.Size(213, 21);
-            this.dropdownProfiles.TabIndex = 1;
-            // 
-            // flowLayoutPanelConvertButton
-            // 
-            this.flowLayoutPanelConvertButton.Controls.Add(this.buttonConvert);
-            this.flowLayoutPanelConvertButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanelConvertButton.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-            this.flowLayoutPanelConvertButton.Location = new System.Drawing.Point(222, 426);
-            this.flowLayoutPanelConvertButton.Name = "flowLayoutPanelConvertButton";
-            this.flowLayoutPanelConvertButton.Size = new System.Drawing.Size(213, 42);
-            this.flowLayoutPanelConvertButton.TabIndex = 2;
-            // 
-            // buttonConvert
-            // 
-            this.buttonConvert.Location = new System.Drawing.Point(135, 3);
-            this.buttonConvert.Name = "buttonConvert";
-            this.buttonConvert.Size = new System.Drawing.Size(75, 23);
-            this.buttonConvert.TabIndex = 0;
-            this.buttonConvert.Text = "Convert -->";
-            this.buttonConvert.UseVisualStyleBackColor = true;
-            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
-            // 
-            // checklistMovieFiles
-            // 
-            this.checklistMovieFiles.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.checklistMovieFiles.FormattingEnabled = true;
-            this.checklistMovieFiles.Location = new System.Drawing.Point(441, 3);
-            this.checklistMovieFiles.Name = "checklistMovieFiles";
-            this.tableLayoutPanel1.SetRowSpan(this.checklistMovieFiles, 2);
-            this.checklistMovieFiles.Size = new System.Drawing.Size(213, 417);
-            this.checklistMovieFiles.TabIndex = 3;
+            this.populateDrives.Location = new System.Drawing.Point(3, 3);
+            this.populateDrives.Name = "populateDrives";
+            this.populateDrives.Size = new System.Drawing.Size(75, 23);
+            this.populateDrives.TabIndex = 6;
+            this.populateDrives.Text = "Refresh";
+            this.populateDrives.UseVisualStyleBackColor = true;
+            this.populateDrives.Click += new System.EventHandler(this.populateDrives_Click);
             // 
             // flowlayoutMoveButton
             // 
@@ -142,6 +83,50 @@
             this.buttonMoveTo.UseVisualStyleBackColor = true;
             this.buttonMoveTo.Click += new System.EventHandler(this.buttonMoveTo_Click);
             // 
+            // flowLayoutPanelConvertButton
+            // 
+            this.flowLayoutPanelConvertButton.Controls.Add(this.buttonConvert);
+            this.flowLayoutPanelConvertButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanelConvertButton.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanelConvertButton.Location = new System.Drawing.Point(222, 426);
+            this.flowLayoutPanelConvertButton.Name = "flowLayoutPanelConvertButton";
+            this.flowLayoutPanelConvertButton.Size = new System.Drawing.Size(213, 42);
+            this.flowLayoutPanelConvertButton.TabIndex = 2;
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(135, 3);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(75, 23);
+            this.buttonConvert.TabIndex = 0;
+            this.buttonConvert.Text = "Convert -->";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.selectListDicom, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dropdownProfiles, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanelConvertButton, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.checklistMovieFiles, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowlayoutMoveButton, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.DriveList, 3, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1.698514F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.1104F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(876, 471);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
             // DriveList
             // 
             this.DriveList.FormattingEnabled = true;
@@ -150,23 +135,38 @@
             this.DriveList.Size = new System.Drawing.Size(213, 409);
             this.DriveList.TabIndex = 5;
             // 
-            // flowLayoutPanel1
+            // checklistMovieFiles
             // 
-            this.flowLayoutPanel1.Controls.Add(this.populateDrives);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(660, 426);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(213, 42);
-            this.flowLayoutPanel1.TabIndex = 6;
+            this.checklistMovieFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checklistMovieFiles.FormattingEnabled = true;
+            this.checklistMovieFiles.Location = new System.Drawing.Point(441, 3);
+            this.checklistMovieFiles.Name = "checklistMovieFiles";
+            this.tableLayoutPanel1.SetRowSpan(this.checklistMovieFiles, 2);
+            this.checklistMovieFiles.Size = new System.Drawing.Size(213, 417);
+            this.checklistMovieFiles.TabIndex = 3;
             // 
-            // populateDrives
+            // dropdownProfiles
             // 
-            this.populateDrives.Location = new System.Drawing.Point(3, 3);
-            this.populateDrives.Name = "populateDrives";
-            this.populateDrives.Size = new System.Drawing.Size(75, 23);
-            this.populateDrives.TabIndex = 6;
-            this.populateDrives.Text = "Refresh";
-            this.populateDrives.UseVisualStyleBackColor = true;
-            this.populateDrives.Click += new System.EventHandler(this.populateDrives_Click);
+            this.dropdownProfiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dropdownProfiles.FormattingEnabled = true;
+            this.dropdownProfiles.Items.AddRange(new object[] {
+            "preset1",
+            "preset2"});
+            this.dropdownProfiles.Location = new System.Drawing.Point(222, 3);
+            this.dropdownProfiles.Name = "dropdownProfiles";
+            this.dropdownProfiles.Size = new System.Drawing.Size(213, 21);
+            this.dropdownProfiles.TabIndex = 1;
+            // 
+            // selectListDicom
+            // 
+            this.selectListDicom.CheckOnClick = true;
+            this.selectListDicom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.selectListDicom.FormattingEnabled = true;
+            this.selectListDicom.Location = new System.Drawing.Point(3, 3);
+            this.selectListDicom.Name = "selectListDicom";
+            this.tableLayoutPanel1.SetRowSpan(this.selectListDicom, 3);
+            this.selectListDicom.Size = new System.Drawing.Size(213, 465);
+            this.selectListDicom.TabIndex = 0;
             // 
             // Form1
             // 
@@ -176,26 +176,27 @@
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Dicom To Movie";
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.flowLayoutPanelConvertButton.ResumeLayout(false);
-            this.flowlayoutMoveButton.ResumeLayout(false);
             this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowlayoutMoveButton.ResumeLayout(false);
+            this.flowLayoutPanelConvertButton.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button populateDrives;
+        private System.Windows.Forms.FlowLayoutPanel flowlayoutMoveButton;
+        private System.Windows.Forms.Button buttonMoveTo;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelConvertButton;
+        private System.Windows.Forms.Button buttonConvert;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.CheckedListBox selectListDicom;
         private System.Windows.Forms.ComboBox dropdownProfiles;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelConvertButton;
-        private System.Windows.Forms.Button buttonConvert;
         private System.Windows.Forms.CheckedListBox checklistMovieFiles;
-        private System.Windows.Forms.FlowLayoutPanel flowlayoutMoveButton;
-        private System.Windows.Forms.Button buttonMoveTo;
-        private System.Windows.Forms.Button populateDrives;
         private System.Windows.Forms.CheckedListBox DriveList;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+
     }
 }
---
+++ cs_proj05_dicom2mov/cs_proj05_dicom2mov/dicom.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dicom;
+using Dicom.Imaging;
+using NReco.VideoConverter;
+
+
+namespace cs_proj05_dicom2mov
+{
+
+
+    class dcm
+    {
+        /*
+            dcm dcm_obj = new dcm(@"C:\Users\sleepingkirby\AppData\Roaming\dicom2mov\dicoms\IM_0016");
+            Console.WriteLine(dcm_obj.patientName);
+            Console.WriteLine(dcm_obj.dateOfScan);
+            Console.WriteLine(dcm_obj.timeOfScan);
+        */
+        public string patientName;
+        public string patientId;
+        public string dicomFilePath;
+        public string dateOfScan;
+        public string timeOfScan; 
+
+        public string frameRate;
+        public int frameNum;
+
+        public string compressFormat;
+        /*
+ISO_10918_1 JPEG Lossy Compression
+ISO_14495_1 JPEG-LS Near-lossless Compression
+ISO_15444_1 JPEG 2000 Irreversible Compression
+ISO_13818_2 MPEG2 Compression
+ISO_14496_10 MPEG-4 AVC/H.264 Compression
+         */
+
+        public dcm(string path)
+        {
+            /*
+            DicomFile dcm_obj = new Dicom.DicomFile();
+            or DicomFile dcm_obj = DicomFile.Open(path);
+            dcm_obj.Dataset.Add(DicomTag.TextString, "test");
+
+                  string test = "hangs";
+                  test = dcm_obj.Dataset.Get<string>(DicomTag.TextString);
+                  Console.WriteLine("--------------------- dicom file ---------------------------");
+                  Console.WriteLine(test);
+              --------------------- dicom file ---------------------------
+                test
+      
+             */
+
+            dicomFilePath = path;
+
+            var image = new DicomImage(path);
+            frameNum = image.NumberOfFrames;
+
+            DicomFile dcm_obj = DicomFile.Open(path);
+
+            patientName = dcm_obj.Dataset.Get<string>(DicomTag.PatientName);
+            frameRate = dcm_obj.Dataset.Get<string>(DicomTag.CineRate);
+            patientId = dcm_obj.Dataset.Get<string>(DicomTag.PatientID);
+            dateOfScan = dcm_obj.Dataset.Get<string>(DicomTag.InstanceCreationDate);
+            timeOfScan = dcm_obj.Dataset.Get<string>(DicomTag.InstanceCreationTime);
+            compressFormat = compFormat(dcm_obj.Dataset.Get<string>(DicomTag.LossyImageCompressionMethod));
+
+        }
+
+        string compFormat(string str)
+        {
+            if (str == "")
+                return "";
+
+            /*
+             * ISO_10918_1 JPEG Lossy Compression
+ISO_14495_1 JPEG-LS Near-lossless Compression
+ISO_15444_1 JPEG 2000 Irreversible Compression
+ISO_13818_2 MPEG2 Compression
+ISO_14496_10 MPEG-4 AVC/H.264 Compression
+             */
+
+            switch (str)
+            {
+                case "ISO_10918_1":
+                    return "jpeg";
+                case "ISO_14495_1":
+                    return "jpeg-ls";
+                case "ISO_15444_1":
+                    return "jpeg 2000";
+                case "ISO_13818_2":
+                    return "mpeg2";
+                case "ISO_14496_10":
+                    return "mp4 avc/h.264";
+                default:
+                    return "no compression";
+            }
+
+        }
+
+    }
+}
