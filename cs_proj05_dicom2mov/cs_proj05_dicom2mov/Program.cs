b3956dd4f76bde1338ac522a541c4fba164eee94
unknown <sleepingkirby@UFOKirby.megapath.biz>
2015-04-20 20:50:42 -0700

    cmd.cs for command line object. No execution just yet. Just parsed any and all parameters with it's values. Will do commands later when they come
    Former-commit-id: 3b3be0994c2a1a67004943a22b53dd3809449f41

--- cs_proj05_dicom2mov/cs_proj05_dicom2mov/Program.cs
+++ cs_proj05_dicom2mov/cs_proj05_dicom2mov/Program.cs
@@ -13,8 +13,18 @@ namespace cs_proj05_dicom2mov
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+
+
+            if (args.Length >= 1)
+            {
+                var cmdParams = cmd.paramParse(args); 
+                //Dictionary<string, List<string>>. Call via hash of array.
+                //i.e. cs_proj05_dicom2mov.exe -name1 val1 val2 --name2 val3 val4 val5
+                // cmdParams["name1"][0] == val1
+                // cmdParams["name2"][2] == val5
+            }
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
---
+++ cs_proj05_dicom2mov/cs_proj05_dicom2mov/cmd.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_proj05_dicom2mov
+{
+    class cmd
+    {
+        public static Dictionary<string, List<string>> paramParse(string[] args){
+            Dictionary<string, List<string>> parseParam= new Dictionary<string, List<string>>();
+            if (args.Length <= 0)
+            {
+                return parseParam;
+            }
+
+            List<string> tempList = new List<string>();
+            string name="";
+            int i=0;
+            int max=args.Length;
+            foreach (string tempstr in args){
+                if (i==0 && name.Length >0 && tempstr.LastIndexOf('-') >= 0)
+                {
+                    name=noDash(tempstr);
+                }
+                
+                if (tempstr.LastIndexOf('-') == -1)
+                {
+                    tempList.Add(tempstr);
+
+
+                }
+                if (i==(max-1) || (tempstr.LastIndexOf('-') >= 0 && i>0))
+                {
+                    parseParam.Add(name, tempList);
+                    tempList = new List<string>();
+                    name = noDash(tempstr);
+                }
+                i++;
+            }
+            
+            return parseParam;
+        }
+
+        public static string noDash(string str){
+            return str.Substring(str.LastIndexOf('-') + 1); ;
+        }
+
+    }
+}
